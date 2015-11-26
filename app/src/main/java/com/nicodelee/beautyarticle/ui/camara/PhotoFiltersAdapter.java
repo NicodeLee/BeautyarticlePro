0f1f1ce4aa45173cbecbc00a8872cf900a83cf91
NicodeLee <lirizhilirizhi@163.com>
2015-11-26 15:29:59 +0800

    滤镜初步完成，效果待优化

--- app/src/main/java/com/nicodelee/beautyarticle/ui/camara/PhotoFiltersAdapter.java
+++ app/src/main/java/com/nicodelee/beautyarticle/ui/camara/PhotoFiltersAdapter.java
@@ -1,45 +1,82 @@
 package com.nicodelee.beautyarticle.ui.camara;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.nicodelee.beautyarticle.R;
+import com.nicodelee.beautyarticle.utils.L;
+import com.nicodelee.utils.ListUtils;
+import java.util.List;
+import java.util.logging.Logger;
+import jp.co.cyberagent.android.gpuimage.GPUImageFilter;
+import jp.co.cyberagent.android.gpuimage.GPUImageView;
 
-/**
- * Created by froger_mcs on 11.11.14.
- */
-public class PhotoFiltersAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class PhotoFiltersAdapter
+    extends RecyclerView.Adapter<PhotoFiltersAdapter.PhotoFilterViewHolder> {
 
-    private Context context;
-    private int itemsCount = 12;
+  private Context context;
 
-    public PhotoFiltersAdapter(Context context) {
-        this.context = context;
+  List<FilterEffect> filterUris;
+  private Bitmap background;
+
+  public PhotoFiltersAdapter(Context context) {
+    this.context = context;
+  }
+
+  public PhotoFiltersAdapter(Context context, List<FilterEffect> effects, Bitmap backgroud) {
+    filterUris = effects;
+    this.context = context;
+    this.background = backgroud;
+  }
+
+  @Override public PhotoFilterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    final View view =
+        LayoutInflater.from(context).inflate(R.layout.item_bottom_filter, parent, false);
+    return new PhotoFilterViewHolder(view);
+  }
+
+  @Override public void onBindViewHolder(PhotoFilterViewHolder holder, final int position) {
+
+    holder.smallFilter.setTag(position);
+
+    final FilterEffect effect = filterUris.get(position);
+    holder.filterName.setText(effect.getTitle());
+    holder.smallFilter.setImage(background);
+    GPUImageFilter filter = GPUImageFilterTools.createFilterForType(context, effect.getType());
+    holder.smallFilter.setFilter(filter);
+
+    holder.itemView.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        GPUImageFilter filter =
+            GPUImageFilterTools.createFilterForType(context, filterUris.get(position).getType());
+        PhotoProcessActivity.photoProcessActivity.gpuimage.setFilter(filter);
+
+      }
+    });
+  }
+
+  @Override public int getItemCount() {
+    return ListUtils.getSize(filterUris);
+  }
+
+  public static class PhotoFilterViewHolder extends RecyclerView.ViewHolder {
+    @Bind(R.id.small_filter) GPUImageView smallFilter;
+    @Bind(R.id.small_filter_container) LinearLayout smallFilterContainer;
+    @Bind(R.id.filter_name) TextView filterName;
+
+    public final View mView;
+
+    public PhotoFilterViewHolder(View view) {
+      super(view);
+      mView = view;
+      ButterKnife.bind(this, view);
     }
-
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        final View view = LayoutInflater.from(context).inflate(R.layout.item_photo_filter, parent, false);
-        return new PhotoFilterViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
-    }
-
-    @Override
-    public int getItemCount() {
-        return itemsCount;
-    }
-
-    public static class PhotoFilterViewHolder extends RecyclerView.ViewHolder {
-
-        public PhotoFilterViewHolder(View view) {
-            super(view);
-            ButterKnife.bind(this, view);
-        }
-    }
+  }
 }
--- app/src/main/java/com/nicodelee/beautyarticle/ui/camara/PhotoProcessActivity.java
+++ app/src/main/java/com/nicodelee/beautyarticle/ui/camara/PhotoProcessActivity.java
@@ -4,18 +4,25 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.nicodelee.beautyarticle.R;
 import com.nicodelee.beautyarticle.app.APP;
 import com.nicodelee.beautyarticle.app.BaseAct;
+import com.nicodelee.beautyarticle.utils.AndroidUtils;
+import com.nicodelee.beautyarticle.utils.DevicesUtil;
 import com.nicodelee.beautyarticle.utils.L;
 import com.nostra13.universalimageloader.core.assist.ImageSize;
+import java.util.List;
+import jp.co.cyberagent.android.gpuimage.GPUImageFilter;
 import jp.co.cyberagent.android.gpuimage.GPUImageView;
 
 /**
@@ -26,13 +33,15 @@ import jp.co.cyberagent.android.gpuimage.GPUImageView;
 public class PhotoProcessActivity extends BaseAct {
 
   @Bind(R.id.gpuimage) GPUImageView gpuimage;
-  @Bind(R.id.drawing_view_container) RelativeLayout drawingViewContainer;
   @Bind(R.id.list_tools) RecyclerView listTools;
-  @Bind(R.id.toolbar_area) LinearLayout toolbarArea;
-  @Bind(R.id.main_area) LinearLayout mainArea;
   @Bind(R.id.title) TextView title;
   @Bind(R.id.left) ImageView left;
   @Bind(R.id.right) ImageView right;
+
+  //用于预览的小图片
+  private Bitmap smallImageBackgroud;
+
+  static PhotoProcessActivity photoProcessActivity;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -41,7 +50,16 @@ public class PhotoProcessActivity extends BaseAct {
     initView();
   }
 
+  @OnClick({ R.id.left }) public void Click(View view) {
+    switch (view.getId()) {
+      case R.id.left:
+        finish();
+        break;
+    }
+  }
+
   private void initView() {
+    photoProcessActivity = this;
     title.setText("编辑");
     left.setImageResource(R.drawable.ic_arrow_back_white_24dp);
     Intent intent = getIntent();
@@ -49,9 +67,26 @@ public class PhotoProcessActivity extends BaseAct {
     L.e(String.format("uri = %S", uri));
 
     if (uri != null) {
-      Bitmap bitmap =
-          APP.getInstance().imageLoader.loadImageSync(uri + "", new ImageSize(720, 1280));
+      Bitmap bitmap = APP.getInstance().imageLoader.loadImageSync(uri + "");
       gpuimage.setImage(bitmap);
+
+      smallImageBackgroud =
+          APP.getInstance().imageLoader.loadImageSync(uri + "", new ImageSize(120, 120));
     }
+    RelativeLayout.LayoutParams rparams =
+        new RelativeLayout.LayoutParams(DevicesUtil.screenHeight, DevicesUtil.screenWidth);
+    gpuimage.setLayoutParams(rparams);
+    initFilterToolBar();
+  }
+
+  //初始化滤镜
+  private void initFilterToolBar() {
+    final List<FilterEffect> filters = EffectService.getInst().getLocalFilters();
+    final PhotoFiltersAdapter adapter =
+        new PhotoFiltersAdapter(PhotoProcessActivity.this, filters, smallImageBackgroud);
+    listTools.setAdapter(adapter);
+    listTools.setHasFixedSize(true);
+    listTools.setLayoutManager(
+        new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
   }
 }
